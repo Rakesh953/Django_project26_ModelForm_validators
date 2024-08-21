fcf8f3c7aa2860d73c4bca337905813cdfb0ee0d
@Rakesh953 <rakeshmeher953@gmail.com>
2024-08-21 12:19:39 +0530

    first commit

---
+++ project26_ModelForm_validators/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for project26_ModelForm_validators project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project26_ModelForm_validators.settings')
+
+application = get_wsgi_application()
